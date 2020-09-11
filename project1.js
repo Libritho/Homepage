0752f524b978a83066c9c45419d205ae23514ab1
Libritho <65232465+Libritho@users.noreply.github.com>
2020-09-11 13:26:38 +0300

    fetch added

--- project1.js
+++ project1.js
@@ -1,8 +1,10 @@
 window.onload = addListeners;
+let content = {};
 
 function addListeners () {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+    content = document.getElementsByClassName("content")[0];
 
     for (let i = 0; i<slides.length; i++) {
         dots[i].onclick = function () {
@@ -20,10 +22,16 @@ function addListeners () {
             }
         }
     }
-}
-fetch('https://api.unsplash.com/search/photos?client_id=d1qplN8zd1k3SAGdyXJIM5fAA30MkNSBSDRNY3qW8c0&per_page=5&page=1&query=office')
-    .then(function (response) {
-        response.json().then(function (data) {
-            console.log('data', data)
+    fetch('https://api.unsplash.com/search/photos?client_id=d1qplN8zd1k3SAGdyXJIM5fAA30MkNSBSDRNY3qW8c0&per_page=5&page=1&query=office')
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log('data', data)
+                console.log('content', content)
+                for (let i = 0; i < data.results.length; i++) {
+                    const img = document.createElement('img');
+                    img.setAttribute('src', `${data.results[i].urls.small}`)
+                    content.appendChild(img);
+                }
+            })
         })
-    })
+}
